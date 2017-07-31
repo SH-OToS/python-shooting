af4d74f2ead9ac82cd57cbe75f08266b640bf3d5
maTORIx <matorixmatrix@gmail.com>
2017-07-31 10:39:14 +0900

    reverce

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Distutils',
+      version='0.1',
+      description='Python Shooting Game',
+      author='maTORIx',
+      author_email='matorixmatrix@gmail.com',
+      url='http://matorix.tk',
+      packages=['pygame'],
+     )
